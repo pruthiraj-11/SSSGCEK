a46b0843d8a6fbfe1bd0c1f77728a9297e88ffad
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2023-12-01 23:59:49 +0530

    CG

--- app/src/main/java/com/example/sspgcek/MainActivity.java
+++ app/src/main/java/com/example/sspgcek/MainActivity.java
@@ -87,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             chatAdapter.notifyItemInserted(list.size()-1);
         }
-/yytyyyt
         Python python=Python.getInstance();
         final PyObject pyObject=python.getModule("res");
 
