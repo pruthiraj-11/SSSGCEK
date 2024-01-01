8f157a20abf7b3631202670f56b6365be88bad8b
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2024-01-01 18:56:23 +0530

    SD

---
+++ app/src/main/java/com/example/sspgcek/Adapters/PDFAdapter.java
@@ -0,0 +1,24 @@
+package com.example.sspgcek.Adapters;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PDFAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder  onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/sspgcek/Models/PDFModel.java
@@ -0,0 +1,26 @@
+package com.example.sspgcek.Models;
+
+public class PDFModel {
+    String filename, fileurl;
+
+    public PDFModel(String filename, String fileurl) {
+        this.filename = filename;
+        this.fileurl = fileurl;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFileurl() {
+        return fileurl;
+    }
+
+    public void setFileurl(String fileurl) {
+        this.fileurl = fileurl;
+    }
+}
